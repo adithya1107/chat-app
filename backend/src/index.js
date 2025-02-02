43dc68afa8c0b06bfbb54d2978d6d821e516e206
adithya1107 <adigaadithya70@gmail.com>
2025-02-02 12:27:16 +0530

    backend fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5175"], 
+    origin: "http://localhost:5173", 
     credentials: true, 
   })
 );
